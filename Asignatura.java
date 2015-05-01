b5d50e127d40dc61fa10658f5da784d98352b5a1
Violeta Feliciano Sanjuan <violetaonil@hotmail.com>
2015-05-01 22:20:16 +0200

    implementar clase Asignatura

--- Asignatura.java
+++ Asignatura.java
@@ -2,4 +2,42 @@ package unit8.unit8;
 
 public class Asignatura {
 
+	private String nombre;
+	private double nota;
+	private String DNI;
+	
+	public Asignatura(String nombre){
+		
+	}
+	
+	public String getNombre(){
+		
+		return nombre;
+	}
+	
+	public void setNombre(String nombre){
+		
+		this.nombre = nombre;
+	}
+	
+	public double getNota(){
+		
+		return nota;
+	}
+	
+	public void setNota(double nota){
+		
+		this.nota = nota;
+	}
+	
+	public String getDni(){
+		
+		return DNI;
+	}
+	
+	public void setDni(String DNI){
+		
+		this.DNI = DNI;
+	}
+	
 }
